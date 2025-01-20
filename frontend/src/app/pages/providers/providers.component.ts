ed46e3698726a2c04dacb61056e6375a95e9523e
PaolaCM99 <paoalm1@hotmail.com>
2025-01-19 20:19:39 -0500

    feat: Se añade primeras paginas

---
+++ frontend/src/app/pages/providers/providers.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-providers',
+  imports: [],
+  template: './providers.component.html',
+  styleUrl: './providers.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProvidersComponent { }
